b323c5aa133afaa17e499bd2f90462814dbd8178
rigo <rigobertoramirez606@gmail.com>
2018-06-21 10:06:40 -0500

    FirstCommit

---
+++ DataStructureImplimintation/Contestant/Contestant.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureImplimintation
+{
+    public class Contestant
+    {
+        private string firstName;
+        private string lastName;
+        private string email;
+        private string adress;
+        private string registrationNumber;
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                firstName = value;
+            }
+        }
+
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Adress { get; set; }
+        public string RegistrationNumber { get; set; }
+
+
+
+
+
+
+
+
+
+    }
+}
